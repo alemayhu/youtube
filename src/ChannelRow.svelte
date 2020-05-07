346552e1dc63dd5ad8a20db5c694cbc279676733
Alexander Alemayhu <alexander@alemayhu.com>
2020-05-07 22:03:19 +0200

    fix image sizes

--- src/ChannelRow.svelte
+++ src/ChannelRow.svelte
@@ -7,9 +7,9 @@
 
 <div class="bg-gray-100 flex  m-2 rounded p-4">
   <div class="w-1/4">
-    <img class="rounded" alt="thumbnail for channel {name}" src={thumbnail} />
+    <img width="100px" class="rounded" alt="thumbnail for channel {name}" src={thumbnail} />
   </div>
-  <div class="flex-grow self-center text-xl px-4">{description}</div>
+  <div class="flex-grow self-center text-xl px-4 w-full">{description}</div>
   <div class="self-center m-2">
     <a
       class="bg-red-600 p-2 rounded text-white"
